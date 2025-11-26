3abb474f34f5e815e892b85dab818502f4dfc0aa
tungnguyenlam <52458895+tungnguyenlam@users.noreply.github.com>
2025-11-26 15:22:42 +0700

    Mt evals (#15)
    * Added Opus 100 Evaluator, in development
    
    * Completed FloresPlusEvaluator and improved Opus100 evaluator, make ElanMT model does not inherit from the TranslationModel interfaces anymore
    
    * Handle 3 Elan Mt models: Bt, base, tiny based on Marian Mt
    
    * Added batch processing for ElanMt model family
    
    * Make MangaOCR class batch process

--- code/pipeline/OCRModels/MangaOCRModel.py
+++ code/pipeline/OCRModels/MangaOCRModel.py
@@ -1,6 +1,8 @@
 from manga_ocr import MangaOcr
 from PIL import Image
 import gc
+import numpy as np
+from math import floor, ceil
 
 def transform_img_to_PIL(img):
     return Image.fromarray(img)
@@ -12,13 +14,27 @@ class MangaOCRModel:
     def load_model(self):
         self.mocr = MangaOcr()
 
-    def predict(self, img):
+    def predict(self, img, bboxes):
         if self.mocr is None:
             raise TypeError("Model is not loaded yet")
-        elif isinstance(img, Image.Image):
-            return self.mocr(img)
-        else:
-            return self.mocr(transform_img_to_PIL(img))
+        
+        image_rgb = np.array(img)
+        cropped_image_list = []
+
+        for box in bboxes:
+            cropped_image = image_rgb[ floor(box[1]): ceil(box[3]), floor(box[0]): ceil(box[2]), :]
+            cropped_image_list.append(cropped_image)
+
+        text_ocr_list = []
+
+        for cropped_img in cropped_image_list:
+            if isinstance(cropped_img, Image.Image):
+                text = self.mocr(cropped_img)
+            else:
+                text = self.mocr(transform_img_to_PIL(cropped_img))
+            text_ocr_list.append(text)
+
+        return text_ocr_list
         
     def unload_model(self):
         del self.mocr
